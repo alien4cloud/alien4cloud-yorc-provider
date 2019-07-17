d2bf0ffead02a33ad0ca1f1dd7ed9082619d9520
ROSSO, LOIC <loic.rosso@atos.net>
2019-07-17 15:50:40 +0200

    add support for simple resource - pvc in yorc

--- src/main/java/alien4cloud/paas/yorc/modifier/KubernetesTopologyModifier.java
+++ src/main/java/alien4cloud/paas/yorc/modifier/KubernetesTopologyModifier.java
@@ -41,12 +41,13 @@ public class KubernetesTopologyModifier extends TopologyModifierSupport {
     protected static final String YORC_KUBERNETES_TYPES_DEPLOYMENT_RESOURCE = "yorc.nodes.kubernetes.api.types.DeploymentResource";
     protected static final String YORC_KUBERNETES_TYPES_STATEFULSET_RESOURCE = "yorc.nodes.kubernetes.api.types.StatefulSetResource";
     protected static final String YORC_KUBERNETES_TYPES_SERVICE_RESOURCE = "yorc.nodes.kubernetes.api.types.ServiceResource";
+    protected static final String YORC_KUBERNETES_TYPES_SIMPLE_RESOURCE = "yorc.nodes.kubernetes.api.types.SimpleResource";
 
     // A4C K8S resource types defined in org.alien4cloud.plugin.kubernetes.modifier
     public static final String K8S_TYPES_DEPLOYMENT_RESOURCE = "org.alien4cloud.kubernetes.api.types.DeploymentResource";
     public static final String K8S_TYPES_SERVICE_RESOURCE = "org.alien4cloud.kubernetes.api.types.ServiceResource";
     public static final String K8S_TYPES_STATEFULSET_RESOURCE = "org.alien4cloud.kubernetes.api.types.StatefulSetResource";
-
+    public static final String K8S_TYPES_SIMPLE_RESOURCE = "org.alien4cloud.kubernetes.api.types.SimpleResource";
 
     @Resource(name="kubernetes-final-modifier")
     ITopologyModifier alien_kubernetes_modifier;
@@ -98,7 +99,8 @@ public class KubernetesTopologyModifier extends TopologyModifierSupport {
         transformKubernetesResourceTypes(topology,  csar, "service", yorcKubernetesTypesArchiveVersion);
         // Treat statefulset resource types
         transformKubernetesResourceTypes(topology,  csar, "statefulSet", yorcKubernetesTypesArchiveVersion);
-
+        // Treat simple resource types
+        transformKubernetesResourceTypes(topology,  csar, "simple", yorcKubernetesTypesArchiveVersion);
     }
 
     /**
@@ -126,7 +128,11 @@ public class KubernetesTopologyModifier extends TopologyModifierSupport {
             case "statefulSet":
                 sourceResourceType = K8S_TYPES_STATEFULSET_RESOURCE;
                 targetResourceType = YORC_KUBERNETES_TYPES_STATEFULSET_RESOURCE;
-            break;
+                break;
+            case "simple":
+                sourceResourceType = K8S_TYPES_SIMPLE_RESOURCE;
+                targetResourceType = YORC_KUBERNETES_TYPES_SIMPLE_RESOURCE;
+                break;
             default:
                 log.info("Yorc K8S Plugin : currently supported K8S resources are " + "service" + " and " + "deployment");
                 break;
